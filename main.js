7e7e7e793a84ae82f219c60364ff0eeb612c1067
IgnaDev <arcortoon@gmail.com>
2023-04-01 21:27:09 -0300

    feat: router creado, mas adelante se le van a dar funciones mas especificas

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const homeLink = document.getElementById("home-link");
+const catalogoLink = document.getElementById("catalogo-link");
+const nosotrosLink = document.getElementById("nosotros-link");
+const contactoLink = document.getElementById("contacto-link");
+const contenido = document.getElementById("app");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function cargarContenido(ruta) {
+  switch (ruta) {
+    case "home":
+      contenido.innerHTML = "<h1>¡Bienvenido a la página de inicio!</h1>";
+      break;
+    case "catalogo":
+      contenido.innerHTML = "<h1>Este es nuestro catálogo de productos</h1>";
+      break;
+    case "nosotros":
+      contenido.innerHTML = "<h1>Conócenos un poco más</h1>";
+      break;
+    case "contacto":
+      contenido.innerHTML = "<h1>Contacta con nosotros</h1>";
+      break;
+    default:
+      contenido.innerHTML = "<h1>Ruta no válida</h1>";
+  }
+  window.history.pushState({ ruta }, null, ruta);
+}
 
-setupCounter(document.querySelector('#counter'))
+// Agregar los eventos de clic a cada link del navbar
+homeLink.addEventListener("click", () => cargarContenido("home"));
+catalogoLink.addEventListener("click", () => cargarContenido("catalogo"));
+nosotrosLink.addEventListener("click", () => cargarContenido("nosotros"));
+contactoLink.addEventListener("click", () => cargarContenido("contacto"));
+
+window.addEventListener("popstate", event => {
+  const ruta = event.state?.ruta;
+  if (ruta) {
+    cargarContenido(ruta);
+  }
+});
